dbb9da180b7b95ec0ef4629dd887c70635ec7a4f
mandyrush <mandyrush85@gmail.com>
2021-11-13 18:26:14 -0700

    add search functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Stories from './components/stories';
 import "./App.css";
 
 const API_URL = 'http://hn.algolia.com/api/v1/search?tags=front_page';
+const API_SEARCH_URL = 'http://hn.algolia.com/api/v1/search?query=';
 
 function App() {
   const [stories, setStories] = useState([]);
@@ -17,13 +18,21 @@ function App() {
       .catch(error => console.log('Error', error));
   }, [])
 
-  useEffect(() => {
-    console.log('stories', stories);
-  }, [stories])
+  // useEffect(() => {
+  //   console.log('stories', stories);
+  // }, [stories])
+
+  const searchStories = (query) => {
+    console.log('Input Updated', query);
+
+    fetch(API_SEARCH_URL + query)
+      .then(response => response.json())
+      .then(data => setStories(data.hits));
+  }
 
   return (
     <div className="App">
-      <Header />
+      <Header searchStories={searchStories} />
       <Stories stories={stories} />
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import React from "react";
+
 import "../styles/header.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-export default function Header() {
+export default function Header({ searchStories }) {
+    const [query, setQuery] = useState([]);
+
+
+    const handleChange = (event) => {
+        console.log('working', event.target.value);
+
+        setQuery(event.target.value);
+
+        searchStories(query);
+    }
+
     return (
         <div className="search-bar">
             <div className="site-name">
@@ -15,7 +28,7 @@ export default function Header() {
                 </a>
             </div>
             <div className="input-container">
-                <input type="text" placeholder="Search stories by title, url or author" />
+                <input type="text" onChange={(event) => handleChange(event)} placeholder="Search stories by title, url or author" />
                 <FontAwesomeIcon icon={faSearch} className="search-icon" />
             </div>
             <a href="/settings"><FontAwesomeIcon icon={faCog} /> Settings</a>
